4d6773f687afd4d5ef4a0a3582db00fc0a1c27df
Sachin S <tmsachins@gmail.com>
2025-02-27 20:20:31 -0600

    File updated

--- File 01.py
+++ File 01.py
@@ -16,5 +16,41 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        print("Listening.......", end="", flush=True)
+        r.pause_threshold=1.0
+        r.phrase_threshold=0.3
+        r.sample_rate = 48000
+        r.dynamic_energy_threshold=True
+        r.operation_timeout=5
+        r.non_speaking_duration=0.5
+        r.dynamic_energy_adjustment=2
+        r.energy_threshold=4000
+        r.phrase_time_limit = 10
+        # print(sr.Microphone.list_microphone_names())
+        # for i in sr.Microphone.list_microphone_names():
+        #     print(i)
+        # audio = r.listen("Headphones (Realtek(R) Audio)")
+        audio = r.listen(source)
+    try:
+        print("\r" ,end="", flush=True)
+        print("Recognizing......", end="", flush=True)
+        query = r.recognize_google(audio, language='en-in')
+        print("\r" ,end="", flush=True)
+        print(f"User said : {query}\n")
+    except Exception as e:
+        print("Say that again please")
+        return "None"
+    return query
+# command().lower
+if __name__ == "__main__":
+    while True:
+        query = command().lower()
+        print(query)
+        speak("Hello,"+ query)
 
 speak("Hello, I'm JARVIS")
+speak("Hello Sumit, I'm JARVIS")
